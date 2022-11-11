d53eb0c7661cb7a246850d612feb944de6cdf43c
dagiTensay <dagitensay@gmail.com>
2022-11-11 22:44:37 +0300

    easy

---
+++ good-pairs.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def numIdenticalPairs(self, nums):
+        ctr = 0
+        indexes=len(nums)
+        for i in range(indexes):
+            for j in range(i,indexes):
+                if nums[i]==nums[j] and i <j:
+                    ctr+=1
+        return ctr
