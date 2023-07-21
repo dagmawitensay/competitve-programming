df126f9059002dec248b652daa56e08baf457616
dagiTensay <118015472+dagiTensay@users.noreply.github.com>
2023-07-21 18:53:25 +0300

    Time: 46 ms (91.82%), Space: 16.7 MB (19.40%) - LeetHub

---
+++ 0274-h-index/0274-h-index.py
@@ -0,0 +1,15 @@
+class Solution:
+    def hIndex(self, citations: List[int]) -> int:
+        citations.sort()
+        n = len(citations)
+        l, r = 0, n - 1
+        
+        while l <= r:
+            mid = (l + r) // 2
+            h = n - mid
+            if citations[mid] < h:
+                l = mid + 1
+            else:
+                r = mid - 1
+        
+        return n - l
