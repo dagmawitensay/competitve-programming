ffbb012b6c0a24e7055f31bb0c05b2754e5e6d0f
dagiTensay <118015472+dagiTensay@users.noreply.github.com>
2023-08-11 12:09:42 +0300

    Time: 63 ms (89.93%), Space: 16.3 MB (82.27%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.py
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.py
@@ -1,11 +1,14 @@
+from collections import Counter
 class Solution:
     def topKFrequent(self, words: List[str], k: int) -> List[str]:
-        store = {}
-        for word in words:
-            store[word] = store.get(word, 0) + 1
-        
-        print(store)
-        sorted_map = sorted(store.items(), key=lambda x: (-x[1], x[0]))
-        ans = [sorted_map[i][0] for i in range(k)]
+        counted = Counter(words)
+
+        heap = [(-count, word) for word, count in counted.items()]
+        heapq.heapify(heap)
+
+        ans = []
+        for i in range(k):
+            count, word = heapq.heappop(heap)
+            ans.append(word)
         
         return ans
