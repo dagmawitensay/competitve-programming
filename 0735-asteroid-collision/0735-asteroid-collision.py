0b9947f309b3f7f743419ae98f5af01bda93adf6
Dagmawi Tensay <118015472+dagmawitensay@users.noreply.github.com>
2023-11-14 19:56:38 +0300

    Time: 97 ms (39.75%), Space: 17.6 MB (14.67%) - LeetHub

---
+++ 0735-asteroid-collision/0735-asteroid-collision.py
@@ -0,0 +1,16 @@
+class Solution:
+    def asteroidCollision(self, asteroids: List[int]) -> List[int]:
+        stack = []
+        
+        for asteroid in asteroids:
+            while stack and asteroid < 0 < stack[-1]:
+                if abs(asteroid) > stack[-1]:
+                    stack.pop()
+                    continue
+                elif abs(asteroid) == stack[-1]:
+                    stack.pop()
+                break
+            else:
+                stack.append(asteroid)
+        
+        return stack
