3a0fd393cf38a91f03e208a99e6112db510cfb1e
Dagmawi Tensay <118015472+dagmawitensay@users.noreply.github.com>
2023-12-06 22:48:48 +0300

    Added solution for Minimum Number of Operations to Move All Balls to Each Box

---
+++ Pythontrack/week1/minimum-number-of-operations-to-move-all-balls-to-each-box.py
@@ -0,0 +1,11 @@
+class Solution:
+    def minOperations(self, boxes: str) -> List[int]:
+        n = len(boxes)
+        ans = [0] * n
+
+        for i in range(n):
+            for j in range(n):
+                if i != j and boxes[j] == '1':
+                    ans[i] += abs(i - j)
+        
+        return ans
