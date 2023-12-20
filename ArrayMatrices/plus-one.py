2f03d545cfda9d1f4e5760626cc84d8144a76c98
Dagmawi Tensay <118015472+dagmawitensay@users.noreply.github.com>
2023-12-20 21:54:20 +0300

    Added solution for Plus One

---
+++ ArrayMatrices/plus-one.py
@@ -0,0 +1,5 @@
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        val = int("".join(map(str, digits)))
+        val += 1
+        return [int(i) for i in list(str(val))]
