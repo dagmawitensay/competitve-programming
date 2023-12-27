c4cae9c8c7611cf569a97d63e496b7a222bd10c7
Dagmawi Tensay <118015472+dagmawitensay@users.noreply.github.com>
2023-12-27 18:59:56 +0300

    Added solution for Intersection of Two Arrays II

---
+++ Sorting/intersection-of-two-arrays-ii.py
@@ -0,0 +1,18 @@
+class Solution:
+    def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        nums1.sort()
+        nums2.sort()
+        common = []
+        l, r = 0, 0
+
+        while l < len(nums1) and r < len(nums2):
+            if nums1[l] == nums2[r]:
+                common.append(nums1[l])
+                l += 1
+                r += 1
+            elif nums1[l] < nums2[r]:
+                l += 1
+            else:
+                r += 1
+        
+        return common
