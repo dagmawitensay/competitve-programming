68fabdc534eb9cb5e71f23a6383eefb56ca93a10
Dagmawi Tensay <118015472+dagmawitensay@users.noreply.github.com>
2023-12-29 18:14:24 +0300

    Added solution for Reduction Operations to Make the Array Elements Equal

--- Sorting/reduction-operations-to-make-the-array-elements-equal.py
+++ Sorting/reduction-operations-to-make-the-array-elements-equal.py
@@ -2,16 +2,13 @@ class Solution:
     def reductionOperations(self, nums: List[int]) -> int:
         nums.sort()
         visited = 0
-        indexes = []
+        ans = 0
+        n = len(nums)
         for i, val in enumerate(nums):
             if val != visited:
                 visited = val
-                indexes.append(i)
-        
-        ans = 0
-        n = len(nums)
-        for i in range(len(indexes) - 1, 0, -1):
-            ans += (n - indexes[i])
+                if i != 0:
+                    ans += (n - i)
 
         return ans
             
