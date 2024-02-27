c5f5c3fe6f2bcd50e40f18a5d522759bde478fcc
Dagmawi Tensay <118015472+dagmawitensay@users.noreply.github.com>
2024-02-27 11:00:22 +0300

    Add solution for Combination Sum

---
+++ camp/week3/leetcode/combination-sum.py
@@ -0,0 +1,20 @@
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        ans = []
+        combination = []
+
+        def backtrack(index):
+            if sum(combination) == target:
+                ans.append(combination.copy())
+                return
+            
+            if sum(combination) > target or index >= len(candidates):
+                return
+            
+            for i in range(index, len(candidates)):
+                combination.append(candidates[i])
+                backtrack(i)
+                combination.pop()
+            
+        backtrack(0)
+        return ans
