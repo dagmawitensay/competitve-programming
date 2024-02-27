e7ff035ce45ed3908d0204643dbef4477183b4b7
Dagmawi Tensay <118015472+dagmawitensay@users.noreply.github.com>
2024-02-27 09:53:37 +0300

    Add solution for Fair Distribution of Cookies

---
+++ camp/week3/leetcode/fair-distribution-of-cookies.py
@@ -0,0 +1,23 @@
+class Solution:
+    def distributeCookies(self, cookies: List[int], k: int) -> int:
+        bucket = [0] * k
+        self.minUnfairness = float('inf')
+        
+        
+        def backtrack(idx):
+            if idx >= len(cookies):
+                self.minUnfairness = min(self.minUnfairness, max(bucket))
+                return
+            
+            if self.minUnfairness <= max(bucket):
+               return
+            
+            for j in range(k):
+                bucket[j] += cookies[idx]
+                if self.minUnfairness > bucket[j]:
+                    backtrack(idx + 1)
+                bucket[j] -= cookies[idx]
+            
+        backtrack(0)
+        
+        return self.minUnfairness
