9121d481c139036f0337c5a433e3e5d44d8db480
Dagmawi Tensay <118015472+dagmawitensay@users.noreply.github.com>
2024-05-08 17:43:55 +0300

    Created 'Cirno's Perfect Bitmasks Classroom' by A2SV Hub

---
+++ 08-May-2024/Cirno's Perfect Bitmasks Classroom 143421.py
@@ -0,0 +1,12 @@
+# Problem: Cirno's Perfect Bitmasks Classroom - https://codeforces.com/problemset/problem/1688/A
+
+t = int(input())
+for _ in range(t):
+    x = int(input())
+
+    val = x & -x
+    while (not (val & x) or not(val ^ x)):
+        val += 1
+    
+    print(val)
+    
