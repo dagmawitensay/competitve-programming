1ed9249a8f0208748bfcb7d5b9ab01de8c01e2e2
Dagmawi Tensay <118015472+dagmawitensay@users.noreply.github.com>
2024-05-11 17:04:40 +0300

    Created 'D - Maximum Number of Zeroes' by A2SV Hub

---
+++ A2SV G5 - Contest #10 11-May-2024/D - Maximum Number of Zeroes 146824.py
@@ -0,0 +1,36 @@
+# Problem: D - Maximum Number of Zeroes - https://codeforces.com/gym/514644/problem/D
+
+from collections import Counter
+import math
+from fractions import Fraction
+
+n = int(input())
+a = list(map(int, input().split()))
+b = list(map(int, input().split()))
+counts = Counter()
+ans = 0
+
+for i in range(n):
+    if a[i] != 0:
+        common = math.gcd(abs(a[i]), abs(b[i]))
+        x = b[i] //  common
+        y = a[i] // common
+        sign = 1
+        if b[i] < 0:
+            x *= -1
+            y *= -1
+        elif b[i] == 0 and a[i] < 0:
+            y *= -1
+        
+        counts[(x, y)] += 1
+    else:
+        if b[i] == 0:
+            ans += 1
+
+    
+
+
+if len(counts) == 0:
+    print(ans)
+else:
+    print(max(counts.values()) + ans)
