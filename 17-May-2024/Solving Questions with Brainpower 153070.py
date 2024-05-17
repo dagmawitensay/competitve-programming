f8aa54542f85370f994f887b28e13af878a55423
Dagmawi Tensay <118015472+dagmawitensay@users.noreply.github.com>
2024-05-17 17:22:34 +0300

    Created 'Solving Questions with Brainpower' by A2SV Hub

---
+++ 17-May-2024/Solving Questions with Brainpower 153070.py
@@ -0,0 +1,12 @@
+# Problem: Solving Questions with Brainpower - https://leetcode.com/problems/solving-questions-with-brainpower/
+
+class Solution:
+    def mostPoints(self, questions: List[List[int]]) -> int:
+        @cache
+        def dp(index):
+            if index >= len(questions):
+                return 0
+            
+            return max(questions[index][0] + dp(index + questions[index][1] + 1), dp(index + 1))
+        
+        return dp(0)
