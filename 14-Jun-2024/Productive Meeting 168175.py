b366036bec00368957dd33ca230cb4a0be02b7dd
Dagmawi Tensay <118015472+dagmawitensay@users.noreply.github.com>
2024-06-14 11:33:59 +0300

    Created 'Productive Meeting' by A2SV Hub

---
+++ 14-Jun-2024/Productive Meeting 168175.py
@@ -0,0 +1,30 @@
+# Problem: Productive Meeting - https://codeforces.com/contest/1579/problem/D
+
+from heapq import heappush, heappop
+
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    arr = list(map(int, input().split()))
+    heap = []
+    ans = []
+    maxTalk = 0
+
+    for i, val in enumerate(arr):
+        heappush(heap, (-val, i))
+    
+
+    while len(heap) > 1:
+        firstVal, firstIndex = heappop(heap)
+        secondVal, secondIndex = heappop(heap)
+        if firstVal != 0 and secondVal != 0:
+            ans.append((firstIndex + 1, secondIndex + 1))
+            maxTalk += 1
+            heappush(heap, (firstVal + 1, firstIndex))
+            heappush(heap, (secondVal + 1, secondIndex))
+    
+
+    print(maxTalk)
+    for val in ans:
+        print(" ".join(map(str, val)))
+
