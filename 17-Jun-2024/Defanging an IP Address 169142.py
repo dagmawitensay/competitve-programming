652d4ee0be0499214e9ddbecdd83bc28055619a6
Dagmawi Tensay <118015472+dagmawitensay@users.noreply.github.com>
2024-06-17 18:38:12 +0300

    Created 'Defanging an IP Address' by A2SV Hub

---
+++ 17-Jun-2024/Defanging an IP Address 169142.py
@@ -0,0 +1,7 @@
+# Problem: Defanging an IP Address - https://leetcode.com/problems/defanging-an-ip-address/description/
+
+class Solution:
+    def defangIPaddr(self, address: str) -> str:
+        address = address.split(".")
+
+        return "[.]".join(address)
