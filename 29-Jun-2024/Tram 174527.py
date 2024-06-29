2d07ceb86c719cf655d7db7a4a60a111bf8969b7
Dagmawi Tensay <118015472+dagmawitensay@users.noreply.github.com>
2024-06-29 18:33:31 +0300

    Created 'Tram' by A2SV Hub

---
+++ 29-Jun-2024/Tram 174527.py
@@ -0,0 +1,12 @@
+# Problem: Tram - https://codeforces.com/problemset/problem/116/A
+
+n = int(input())
+min_capacity = 0
+inside_tram = 0
+for _ in range(n):
+    a, b = map(int, input().split())
+    inside_tram -= a
+    inside_tram += b
+    min_capacity = max(min_capacity, inside_tram)
+    
+print(min_capacity)
