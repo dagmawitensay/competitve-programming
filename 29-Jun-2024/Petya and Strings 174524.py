d1338fb224ed9a1f2f325580c9d1f2917903da22
Dagmawi Tensay <118015472+dagmawitensay@users.noreply.github.com>
2024-06-29 18:28:46 +0300

    Created 'Petya and Strings' by A2SV Hub

---
+++ 29-Jun-2024/Petya and Strings 174524.py
@@ -0,0 +1,17 @@
+# Problem: Petya and Strings - https://codeforces.com/problemset/problem/112/A
+
+str1 = input()
+str2 = input()
+
+def solve(str1, str2):
+    for i in range(min(len(str1), len(str2))):
+        if str1[i].lower() < str2[i].lower():
+            return -1
+        elif str1[i].lower() > str2[i].lower():
+            return 1
+    
+    return 0
+
+print(solve(str1, str2))
+
+    
