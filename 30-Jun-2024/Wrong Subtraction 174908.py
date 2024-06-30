24c036fa0fffdc1f6107b82c6fb0dc403d5db19b
Dagmawi Tensay <118015472+dagmawitensay@users.noreply.github.com>
2024-06-30 19:24:47 +0300

    Created 'Wrong Subtraction' by A2SV Hub

---
+++ 30-Jun-2024/Wrong Subtraction 174908.py
@@ -0,0 +1,11 @@
+# Problem: Wrong Subtraction - https://codeforces.com/problemset/problem/977/A?mobile=false
+
+n, k = map(int, input().split())
+
+for i in range(k):
+    if n % 10 != 0:
+        n -= 1
+    else:
+        n //= 10
+    
+print(n)
