db342d3af78a7f7bbbf0ee663e492b4dc9a8c0c2
Dagmawi Tensay <118015472+dagmawitensay@users.noreply.github.com>
2024-07-29 23:02:28 +0300

    Created 'Fedor and New Game' by A2SV Hub

---
+++ 29-Jul-2024/Fedor and New Game 185835.py
@@ -0,0 +1,18 @@
+# Problem: Fedor and New Game - https://codeforces.com/contest/467/problem/B
+
+n, m, k = map(int, input().split())
+count = 0
+players = []
+
+for i in range(m + 1):
+    j = int(input())
+    players.append(j)
+
+fedor = players[-1]
+for i in range(m):
+    xor = players[i] ^ fedor
+    if xor.bit_count() <= k:
+        count += 1
+
+
+print(count)
