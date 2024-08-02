7ab706103142256e215f435c948a78644a92988e
Dagmawi Tensay <118015472+dagmawitensay@users.noreply.github.com>
2024-08-02 17:12:54 +0300

    Created 'Heap Operations' by A2SV Hub

---
+++ 02-Aug-2024/Heap Operations 186790.py
@@ -0,0 +1,39 @@
+# Problem: Heap Operations - https://codeforces.com/contest/681/problem/C
+
+from heapq import heappush, heappop
+
+operations = []
+heap = []
+n = int(input())
+for i in range(n):
+    operation = input()
+    splitted = operation.split()
+    if len(splitted) > 1:
+        op, val = splitted
+        if op == "insert":
+            heappush(heap, int(val))
+        
+        else:
+            while heap and heap[0] < int(val):
+                heappop(heap)
+                operations.append("removeMin")
+            
+            if (heap and heap[0] != int(val) )or not heap:
+                operations.append("insert " + val)
+                heappush(heap, int(val))
+    else:
+        if not heap:
+            operations.append("insert 2")
+        else:
+            heappop(heap)
+
+    operations.append(operation)
+
+print(len(operations))
+for operation in operations:
+    print(operation)
+
+
+
+
+            
