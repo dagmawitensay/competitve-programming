b7d88ed79ca2220b0c5231123d386147cb20f0d2
Dagmawi Tensay <118015472+dagmawitensay@users.noreply.github.com>
2026-01-09 15:47:53 +0300

    Time: 30 ms (84.12%), Space: 20.1 MB (8.21%) - LeetHub

--- 0707-design-linked-list/0707-design-linked-list.py
+++ 0707-design-linked-list/0707-design-linked-list.py
@@ -4,23 +4,29 @@ class MyLinkedList:
         self.head = None
         self.tail = None
         self.size = 0
+        
 
     def get(self, index: int) -> int:
-        if index < 0 or index >= self.size:
+        if index >= self.size:
             return -1
+        
         current = self.head
+
         for i in range(index):
             current = current.next
+        
         return current.val
 
     def addAtHead(self, val: int) -> None:
         new_node = Node(val)
-        if self.size == 0:
+        if not self.head:
             self.head = self.tail = new_node
-        else:
-            new_node.next = self.head
-            self.head = new_node
+        
+        new_node.next = self.head
+        self.head = new_node
+
         self.size += 1
+        
 
     def addAtTail(self, val: int) -> None:
         new_node = Node(val)
@@ -29,27 +35,33 @@ class MyLinkedList:
         else:
             self.tail.next = new_node
             self.tail = new_node
+        
         self.size += 1
 
     def addAtIndex(self, index: int, val: int) -> None:
-        if index < 0 or index > self.size:
+        if index > self.size:
             return
+        
         if index == 0:
             self.addAtHead(val)
         elif index == self.size:
             self.addAtTail(val)
         else:
-            new_node = ListNode(val)
+            new_node = Node(val)
             current = self.head
-            for i in range(index - 1):
+
+            for i in range(index -1):
                 current = current.next
+            
             new_node.next = current.next
             current.next = new_node
             self.size += 1
+        
 
     def deleteAtIndex(self, index: int) -> None:
-        if index < 0 or index >= self.size or not self.head:
+        if index >= self.size or not self.head:
             return
+        
         if index == 0:
             self.head = self.head.next
             if self.size == 1:
@@ -58,15 +70,21 @@ class MyLinkedList:
             current = self.head
             for i in range(index - 1):
                 current = current.next
+            
             current.next = current.next.next
             if index == self.size - 1:
                 self.tail = current
+        
         self.size -= 1
+    
+
 class Node:
-    def __init__(self, val):
+    def __init__(self, val, next=None):
         self.val = val
         self.next = next
+        
 
+    
 # Your MyLinkedList object will be instantiated and called as such:
 # obj = MyLinkedList()
 # param_1 = obj.get(index)
